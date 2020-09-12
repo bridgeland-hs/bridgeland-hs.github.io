391f18cf722f5c853ec95f52ef0065e83c1c3bda
funnyboy-roks <funnyboyroks@gmail.com>
2020-09-11 20:33:41 -0500

    Finish Schedule

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -21,7 +21,7 @@ const schedule = {
             createClass("4", "10:16", "11:09"),
             createClass("5a", "11:15", "11:44"),
             createClass("lunch", "11:44", "12:14"),
-            createClass("5d", "12:20", "12:50"),
+            createClass("5b", "12:20", "12:50"),
         ],
         "lunch_d": [
             createClass("4", "10:16", "11:09"),
@@ -37,7 +37,7 @@ const schedule = {
     "advisory": ["7:20", "8:06"]
 }
 const currentSchedule = currentDate.getDay() == 3 ? schedule.advisory : schedule.regular;
-const lunch = "c";
+let lunch = "c";
 
 // const schedule = {
 //     regular: ["7:20", "8:11"],
@@ -49,44 +49,128 @@ function timeLeft(d = new Date()) {
     const t = d; // current date
     // Math.round((time("18:00")-t)/60000) //
 
-    if (t.getDay() == 0 || t.getDay() == 6) {
-        console.log("weekend");
+    if (d.getDay() == 0 || d.getDay() == 6) {
+        return {
+            inClass: false,
+            weekend: true,
+            day: d.getDay == 0 ? "Sunday" : "Saturday"
+        };
     } else {
-        if (Math.round((currentSchedule.before_lunch[0].start - t) / 60000) > 0) {
-            console.log(`BEFORE CLASS ${formatTime(Math.round((currentSchedule.before_lunch[0].start - t) / 60000))} Left`);
+        if (Math.round((currentSchedule.before_lunch[0].start - d) / 60000) > 0) {
+            let tl = Math.round((currentSchedule.before_lunch[0].start - d) / 60000);
+            return {
+                inClass: false,
+                before_school: true,
+                after_school: false,
+                tl
+            };
 
+        } else if (Math.round((currentSchedule.after_lunch[1].end - d) / 60000) < 0) {
+            return {
+                inClass: false,
+                before_school: false,
+                after_school: true,
+                tl: 0
+            };
         } else {
-            startTimes = []
-            let currentPeriod;
-            for (var c of currentSchedule.before_lunch) {
-                const minsStart = Math.round((c.start - t) / 60000);
-                const minsEnd = Math.round((c.end - t) / 60000);
-                if(minsStart <= 0 && minsEnd >= 0) currentPeriod = c;
-                if (minsStart > 0) startTimes.push(c);
-                console.log(`${c.name}: ${minsStart}, ${minsEnd}`);
+            let startTimes = []
+            let currentPd = {
+                name: "Passing",
+                inClass: false,
+                unset: true,
+            };
+            let nextPd = {
+                name: "none"
+            };
+            for (var arr of ["before_lunch", "lunch_" + lunch, "after_lunch"]) {
+                for (var c of currentSchedule[arr]) {
+                    const minsStart = Math.round((c.start - d) / 60000);
+                    const minsEnd = Math.round((c.end - d) / 60000);
+                    if (currentPd.unset && minsStart <= 0 && minsEnd >= 0) currentPd = c;
+                    if (minsStart > 0 || minsEnd > 0) startTimes.push(c);
+                    console.log(`${c.name}: ${minsStart}, ${minsEnd}`);
+                }
             }
-            // for (var c of currentSchedule["lunch_" + lunch]) {
-            //     const mins = Math.round((c.start - t) / 60000);
-            //     if (mins > 0) startTimes.push(c);
-            //     console.log(`${c.name}: ${mins}`);
-            // }
-            // for (var c of currentSchedule.after_lunch) {
-            //     const mins = Math.round((c.start - t) / 60000);
-            //     if (mins > 0) startTimes.push(c);
-            //     console.log(`${c.name}: ${mins}`);
-            // }
-            startTimes.sort((a, b) => {a.start-b.start});
+            startTimes.sort((a, b) => {
+                a.start - b.start
+            });
+
+            if (startTimes[0] == currentPd && startTimes.length > 1) {
+                nextPd = startTimes[1];
+            } else {
+                nextPd = startTimes[0];
+            }
             console.log(startTimes);
-            console.log(`Current Period: ${c.name}`);
+            let currentPdTimeLeft = Math.round((currentPd.end - d) / 60000);
+            let nextPdStartsIn = Math.round((nextPd.start - d) / 60000);
+            console.log(`Current Period: ${currentPd.name}, Time Left: ${currentPdTimeLeft}\nNext Period: ${nextPd.name}, Starts in: ${nextPdStartsIn}`);
+            return {
+                inClass: true,
+                currentPd,
+                currentPdTimeLeft,
+                nextPd,
+                nextPdStartsIn
+            }
         }
     }
 
 }
 
-function time(time) {
-    // return new Date(`${currentDate.getFullYear()}-${String(currentDate.getMonth()+1).padStart(2, '0')}-${currentDate.getDate()}T${time}:00`)
-    return new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), time.split(':')[0], time.split(':')[1])
+function timeLoopAndUpdate(d = new Date()) {
+    info = timeLeft(d);
+    // document.querySelector("#time-left").innerText = formatTime(info.currentPdTimeLeft);
+    // document.querySelector("#current-period").innerText = info.currentPd.name;
+    console.log(info);
+    if (!info.inClass) {
+        if (info.weekend) {
+            HTMLOut = `<h2 class="subtitle">It's ${info.day}, there's no school.</h2>`;
+        } else if (info.after_school) {
+            HTMLOut = `<h2 class="subtitle">School's over for today!</h2>`;
+        } else if (info.before_school) {
+            HTMLOut = `<h2 class="subtitle">School hasn't started yet.<h2>
+    <h2 class="subtitle">Next Period:</h2>
+    <h3>1</h3>
+    <h2 class="subtitle">Starts At:</h2>
+    <h3 id="time-left">7:20 (In ${formatTime(info.tl)})</h3>
+    `
+        }
+    } else if (info.currentPd.name=="Passing") {
+        HTMLOut = `
+        <h2 class="subtitle">Period:</h2>
+        <h3>${info.currentPd.name}</h3>
+
+        <h2 class="subtitle">Next Period:</h2>
+        <h3>${info.nextPd.name}</h3>
+        <h2 class="subtitle">Starts At:</h2>
+        <h3 id="time-left">${time(info.nextPd.end)} (In ${formatTime(info.nextPdStartsIn)})</h3>`
+    } else {
+        HTMLOut =
+            `<h2 class="subtitle">Period:</h2>
+    <h3 id="current-period">${info.currentPd.name}</h3>
+    <h2 class="subtitle">Ends At:</h2>
+    <h3 id="time-left">${time(info.currentPd.end)} (${formatTime(info.currentPdTimeLeft)} left)</h3>
+
+    <h2 class="subtitle">Next Period:</h2>
+    <h3>${info.nextPd.name}</h3>
+    <h2 class="subtitle">Starts At:</h2>
+    <h3 id="time-left">${time(info.nextPd.end)} (In ${formatTime(info.nextPdStartsIn)})</h3>
+    `
+    }
+
+    document.querySelector("#schedule-timings").innerHTML = HTMLOut
+
+
 }
+
+function time(t) {
+    // return new Date(`${currentDate.getFullYear()}-${String(currentDate.getMonth()+1).padStart(2, '0')}-${currentDate.getDate()}T${t}:00`)
+    if (typeof t == "string") {
+        return new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), t.split(':')[0], t.split(':')[1])
+    } else {
+        return formatTime(t.getHours() * 60 + t.getMinutes());
+    }
+}
+
 
 function createClass(name, start, end) {
     return {
@@ -99,3 +183,6 @@ function createClass(name, start, end) {
 function formatTime(mins) {
     return `${String(Math.floor(mins/60)).padStart(2, 0)}:${String(mins % 60).padStart(2, 0)}`;
 }
+console.log(`Page Loaded in ${new Date() - currentDate}ms`);
+
+timeLoopAndUpdate();
