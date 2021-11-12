f2a962fdceeea2bdae37fe254e64ee80926b76f1
funnyboy-roks <funnyboyroks@gmail.com>
2021-11-12 09:10:23 -0600

    Add Day Progress bar

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -4,16 +4,15 @@ const clock = document.querySelector('#clock');
 const offsetEl = document.querySelector('#offset');
 const image = document.querySelector('#schedule-img');
 const progressbar = document.querySelector('#progressbar');
+const progressbararea = document.querySelector('#progressbar-area');
+const percent = document.querySelector('#header #percent');
 const defaultImage = '../image/Default_Schedule.jpg';
+const dayProgressBar = document.querySelector('#progress-day')
 
 let offset = 0;
 
 offsetEl.addEventListener('input', (e) => (offset = offsetEl.valueAsNumber));
 
-// hash = location.hash.split("#");
-// if (hash[0].match(/[abc]$/i)) {
-//     lunchSel.value = location.hash[1];
-// }
 const searches = {};
 for (let i of location.search.substr(1).split('&')) {
 	let x = i.split('=');
@@ -88,6 +87,9 @@ function timeLeft(d = new Date()) {
 			};
 		} else {
 			let startTimes = [];
+			let prevPd = {
+				name: 'none',
+			}
 			let currentPd = {
 				name: 'Passing',
 				inClass: false,
@@ -104,10 +106,11 @@ function timeLeft(d = new Date()) {
 				for (let c of currentSchedule[arr]) {
 					const minsStart = (c.start - d) / 1000;
 					const minsEnd = (c.end - d) / 1000;
-					if (currentPd.unset && minsStart <= 0 && minsEnd >= 0)
+					if (currentPd.unset && minsStart <= 0 && minsEnd >= 0) {
+						prevPd = currentPd;
 						currentPd = c;
+					}
 					if (minsStart > 0 || minsEnd > 0) startTimes.push(c);
-					// console.log(`${c.name}: ${minsStart}, ${minsEnd}`);
 				}
 			}
 			startTimes.sort((a, b) => {
@@ -127,13 +130,9 @@ function timeLeft(d = new Date()) {
 					after_school: true,
 				};
 			}
-			// console.log(startTimes);
 			let currentPdTimeLeft = (currentPd.end - d) / 1000;
 			let nextPdStartsIn = (nextPd.start - d) / 1000;
-			updateProgressBar(currentPd.start, currentPd.end, new Date());
-			// console.log(
-			// 	`Current Period: ${currentPd.name}, Time Left: ${currentPdTimeLeft}\nNext Period: ${nextPd.name}, Starts in: ${nextPdStartsIn}`
-			// );
+			updateProgressBar(currentPd.start || prevPd.end, currentPd.end || nextPd.start, new Date(), currentPd);
 			return {
 				inClass: true,
 				currentPd,
@@ -168,10 +167,10 @@ function timeLoopAndUpdate(d = new Date()) {
 
 	if (!info.inClass) {
 		if (info.weekend) {
-			HTMLOut = `<h2 class="subtitle">It's ${info.day}, there's no school.</h2>`;
+			HTMLOut = `<h2 class="subtitle">It's the weekend, there's no school.</h2>`;
 			document.querySelector(
 				'title'
-			).innerText = `It's ${info.day}, there's no school.`;
+			).innerText = `It's the weekend, there's no school.`;
 		} else if (info.after_school) {
 			HTMLOut = `<h2 class="subtitle">School's over for today!</h2>`;
 			document.querySelector(
@@ -180,74 +179,43 @@ function timeLoopAndUpdate(d = new Date()) {
 		} else if (info.before_school) {
 			HTMLOut = `<h2 class="subtitle">School hasn't started yet.<h2>
     <h2 class="subtitle">Next Period:</h2>
-    <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text">1</h3>
+    <h3 class="output-text">1</h3>
     <h2 class="subtitle">Starts At:</h2>
-    <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text" id="time-left">7:20 (In ${formatTime(info.tl)})</h3>
+    <h3 class="output-text" id="time-left">7:20 (In ${formatTime(info.tl)})</h3>
     `;
-			document.querySelector(
-				'title'
-			).innerText = `School Hasn't Started Yet!`;
+			document.querySelector('title').innerText = `School Hasn't Started Yet!`;
 		}
 	} else if (info.currentPd.name == 'Passing') {
-		document.querySelector('title').innerText = `Period ${
-			info.nextPd.name
-		} starts in ${formatTime(info.nextPdStartsIn)}`;
+		document.querySelector('title').innerText = `Period ${info.nextPd.name
+			} starts in ${formatTime(info.nextPdStartsIn)}`;
 		HTMLOut = `
         <h2 class="subtitle">Period:</h2>
-        <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text">${info.currentPd.name}</h3>
+        <h3 class="output-text">${info.currentPd.name}</h3>
 
         <h2 class="subtitle">Next Period:</h2>
-        <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text">${info.nextPd.name}</h3>
+        <h3 class="output-text">${info.nextPd.name}</h3>
         <h2 class="subtitle">Starts At:</h2>
-        <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text" id="time-left">${time(info.nextPd.start, true)} (In ${formatTime(
+        <h3 class="output-text" id="time-left">${time(info.nextPd.start, true)} (In ${formatTime(
 			info.nextPdStartsIn
 		)})</h3>`;
 	} else {
 		HTMLOut = `<h2 class="subtitle">Period:</h2>
-    <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text" id="current-period">${info.currentPd.name}</h3>
+    <h3 class="output-text" id="current-period">${info.currentPd.name}</h3>
     <h2 class="subtitle">Ends At:</h2>
-    <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text" id="time-left">${time(info.currentPd.end, true)} (${formatTime(
+    <h3 class="output-text" id="time-left">${time(info.currentPd.end, true)} (${formatTime(
 			info.currentPdTimeLeft
 		)} left)</h3>`;
 		if (info.nextPd.inClass != false) {
 			HTMLOut += `<h2 class="subtitle">Next Period:</h2>
-    <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text">${info.nextPd.name}</h3>
+    <h3 class="output-text">${info.nextPd.name}</h3>
     <h2 class="subtitle">Starts At:</h2>
-    <h3 class="output-text"
- class="output-text"
- class="output-text"
- class="output-text" id="time-left">${time(info.nextPd.start, true)} (In ${formatTime(
+    <h3 class="output-text" id="time-left">${time(info.nextPd.start, true)} (In ${formatTime(
 				info.nextPdStartsIn
 			)})</h3>
     `;
 		}
-		document.querySelector('title').innerText = `Period ${
-			info.currentPd.name
-		} : ${formatTime(info.currentPdTimeLeft)}`;
+		document.querySelector('title').innerText = `Period ${info.currentPd.name
+			} : ${formatTime(info.currentPdTimeLeft)}`;
 	}
 	document.querySelector('#schedule').innerHTML = HTMLOut;
 	updateClock();
@@ -257,27 +225,97 @@ function updateClock() {
 	const date = new Date();
 
 	const hr = date.getHours() > 12 && searches.twelveHour ? date.getHours() - 12 : date.getHours();
-	
+
 	const hour = ('' + hr).padStart(2, 0);
 	const mins = ('' + date.getMinutes()).padStart(2, 0);
 	const secs = ('' + date.getSeconds()).padStart(2, 0);
 	const ampm = searches.twelveHour ? (date.getHours() < 12 ? ' <small>AM</small>' : ' <small>PM</small>') : '';
 
 
-	clock.innerHTML = `<br><br><h2 class="subtitle" id="clock">${hour}:${mins}:${secs}${ampm}</h2>`;
+	clock.innerHTML = `${hour}:${mins}:${secs}${ampm}`;
 }
 
-/**
- * 
- * @param {Date} initial 
- * @param {Date} final 
- * @param {Date} current 
- */
-function updateProgressBar(initial, final, current) {
-	const length = final.valueOf() - initial.valueOf();
-	const currentS = current.valueOf() - initial.valueOf();
-	progressbar.style = 'width: ' + (currentS / length) * 100 + '%';
+function updateProgressBar(initial, final, current, currentPd) {
+	try {
+		const length = final.valueOf() - initial.valueOf();
+		const currentS = current.valueOf() - initial.valueOf();
+		const perc = (currentS / length) * 100
+		if (currentPd.name == 'none') {
+			progressbar.style = 'width: 0%';
+			progressbararea.title = ''
+			return;
+		};
+		progressbar.style = 'width: ' + perc + '%';
+		progressbararea.title = Math.round(perc) + '%';
+	} catch (e) {
+		progressbar.style = 'width: 0%';
+		progressbararea.title = ''
+	}
+	updateDayProgressBar()
+}
 
+function updateDayProgressBar(time = new Date()) {
+	const dayStart = currentSchedule.before_lunch[0].start.valueOf();
+	const dayEnd = currentSchedule.after_lunch[1].end.valueOf();
+	current = (time.valueOf() / 1000) - (dayStart / 1000);
+	const dayLength = (dayEnd - dayStart) / 1000;
+	periods = [];
+	let prev = null;
+	[
+		'before_lunch',
+		'lunch_' + searches.lunch,
+		'after_lunch',
+	].forEach(l => currentSchedule[l].forEach(pd => {
+		const end = (pd.end.valueOf() - dayStart) / 1000;
+		const start = (pd.start.valueOf() - dayStart) / 1000;
+		const length = end - start;
+		const width = length / dayLength;
+		if (prev != null && prev.end != start) {
+			periods.push({
+				name: 'Passing',
+				start: prev.end,
+				end: start,
+				length: start - prev.end,
+				width: (start - prev.end) / dayLength,
+			});
+		}
+		const per = {
+			name: pd.name,
+			start,
+			end,
+			length,
+			width
+		}
+		periods.push(per);
+		prev = per;
+	}));
+
+	dayProgressBar.innerHTML = '';
+	for (const period of periods) {
+		const passing = period.name === 'Passing';
+		if (period.start < current && period.end < current) { // Completed periods
+
+			dayProgressBar.innerHTML += `<div
+			 class="progress-bar bg-${passing ? 'info' : 'primary'} pd"
+			 title="${period.name}"
+			 style="width: ${period.width * 100}%;">
+			 ${passing ? '' : titleCase(period.name)}
+			 </div>`
+		} else if (period.start < current && period.end > current) { // Partial Bar
+			const l = period.end - period.start;
+			const c = current - period.start;
+			const t = c / l;
+			dayProgressBar.innerHTML += `<div
+			 class="progress-bar bg-${period.name === 'Passing' ? 'info' : 'primary'} progress-bar-striped progress-rounded pd"
+			 title="${period.name}"
+			 style="width: ${period.width * 100 * t}%;">
+			 ${passing ? '' : titleCase(period.name)}
+			 </div>`
+		}
+
+	}
+
+	// periods.forEach()
 }
 
 timeLoopAndUpdate();
